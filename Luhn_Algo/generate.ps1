f298be441eb83abd838a6de673edf7c5848af8e4
FonderElite <s1170044@usls.edu.ph>
2022-06-07 17:10:17 +0800

    Update generate.ps1

--- Luhn_Algo/generate.ps1
+++ Luhn_Algo/generate.ps1
@@ -1,5 +1,6 @@
 $i = 0;
-$luhnAlgoLoc = Read-Host -Prompt 'Directory Location of Luhn-Algo'
+$param = $args[0];
+$luhnAlgoLoc = Read-Host -Prompt 'Directory Location of Luhn-Algo';
 python "$luhnAlgoloc\Luhn_Algo\banner.py"
  for($i = 0; $i -lt $param; $i++){ 
    python "$luhnAlgoloc\Luhn_Algo\luhn_prototype.py"

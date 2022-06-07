12ca9a166fce2e5c828f781366219e694662343e
FonderElite <s1170044@usls.edu.ph>
2022-06-07 05:13:15 -0400

    Add files via upload

---
+++ generate.ps1
@@ -0,0 +1,8 @@
+$i = 0;
+$param = $args[0]
+$luhnAlgoLoc = Read-Host -Prompt 'Directory Location of Luhn-Algo'
+python "$luhnAlgoloc\Luhn_Algo\banner.py"
+ for($i = 0; $i -lt $param; $i++){ 
+   python "$luhnAlgoloc\Luhn_Algo\luhn_prototype.py"
+}
+

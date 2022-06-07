457d7e582d13a359cb060300da863bae2bf7ecf6
FonderElite <s1170044@usls.edu.ph>
2022-06-07 17:11:51 +0800

    Delete Luhn_Algo directory

--- Luhn_Algo/generate.ps1
+++
@@ -1,8 +0,0 @@
-$i = 0;
-$param = $args[0];
-$luhnAlgoLoc = Read-Host -Prompt 'Directory Location of Luhn-Algo';
-python "$luhnAlgoloc\Luhn_Algo\banner.py"
- for($i = 0; $i -lt $param; $i++){ 
-   python "$luhnAlgoloc\Luhn_Algo\luhn_prototype.py"
-}
-
